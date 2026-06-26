43a506d574fe9766a0d9a61b96eb39c7dc0abc31
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-06-26 03:03:17 -0400

    Use valid territory counts in Asset Bank tests

--- src/state/asset-bank.test.ts
+++ src/state/asset-bank.test.ts
@@ -10,13 +10,13 @@ function createAssetGame() {
         id: 'player_1',
         name: 'Player One',
         deck: ['card-fortifications', 'p1-card-2', 'p1-card-3', 'p1-card-4'],
-        territories: ['p1-territory-1'],
+        territories: ['p1-territory-1', 'p1-territory-2', 'p1-territory-3'],
       },
       {
         id: 'player_2',
         name: 'Player Two',
         deck: ['p2-card-1', 'p2-card-2', 'p2-card-3', 'p2-card-4'],
-        territories: ['p2-territory-1'],
+        territories: ['p2-territory-1', 'p2-territory-2', 'p2-territory-3'],
       },
     ],
   }));
