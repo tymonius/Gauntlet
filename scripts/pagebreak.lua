e48b20111de2477d4c2c0f581728450a14d3d802
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-07-21 13:00:29 -0400

    Honor raw HTML page breaks in DOCX output

--- scripts/pagebreak.lua
+++ scripts/pagebreak.lua
@@ -1,19 +1,31 @@
+local function page_break_block()
+  if FORMAT:match('docx') then
+    return pandoc.RawBlock('openxml', '<w:p><w:r><w:br w:type="page"/></w:r></w:p>')
+  elseif FORMAT:match('html') then
+    return pandoc.RawBlock('html', '<div class="page-break"></div>')
+  elseif FORMAT:match('latex') then
+    return pandoc.RawBlock('latex', '\\newpage')
+  end
+  return {}
+end
+
 function Div(el)
   if el.classes:includes('docx-page-break') then
     if FORMAT:match('docx') then
-      return pandoc.RawBlock('openxml', '<w:p><w:r><w:br w:type="page"/></w:r></w:p>')
+      return page_break_block()
     end
     return {}
   end
 
   if el.classes:includes('page-break') then
-    if FORMAT:match('docx') then
-      return pandoc.RawBlock('openxml', '<w:p><w:r><w:br w:type="page"/></w:r></w:p>')
-    elseif FORMAT:match('html') then
-      return pandoc.RawBlock('html', '<div class="page-break"></div>')
-    elseif FORMAT:match('latex') then
-      return pandoc.RawBlock('latex', '\\newpage')
-    end
+    return page_break_block()
+  end
+  return el
+end
+
+function RawBlock(el)
+  if el.format == 'html' and el.text:match('<div%s+[^>]*class=["\'][^"\']*page%-break[^"\']*["\'][^>]*>%s*</div>') then
+    return page_break_block()
   end
   return el
 end
