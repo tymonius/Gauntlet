aaf677ee138edab82f8b7f48ff70aeb786656d5f
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-07-21 15:56:17 -0400

    Export canonical v0.6 setup API

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,6 +1,7 @@
 export * from './views';
 export * from './initialize';
 export * from './validation';
+export * from './v06-setup';
 export * from './actions';
 export * from './draw';
 export { applyGameAction } from './apply';
