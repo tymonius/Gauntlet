4bd09ff093a16eb78063e067c802a38a9e1d6cb0
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-07-22 23:42:06 -0400

    Use raster leader portraits in faction sheets

---
+++ faction-sheets/leader-portrait-sources.js
@@ -0,0 +1,29 @@
+(() => {
+  const portraitSources = {
+    Banker: "../images/banker.png",
+    Executive: "../images/executive.png",
+    Alchemist: "../images/alchemist.png",
+    "Spirit Walker": "../images/spirit walker.png",
+    Ranger: "../images/leader-cards/ranger.jpg",
+    Spymaster: "../images/leader-cards/spymaster.jpg"
+  };
+
+  document.querySelectorAll(".leader-card").forEach(card => {
+    const title = card.dataset.cardName || card.querySelector(".leader-title")?.textContent || "";
+    const normalizedTitle = title.trim().replace(/\s+/g, " ").replace(/^(.+)$/u, value =>
+      value.toLowerCase().replace(/\b\w/g, character => character.toUpperCase())
+    );
+    const source = portraitSources[normalizedTitle];
+    if (!source) return;
+
+    let image = card.querySelector(".leader-art img");
+    if (!image) {
+      image = document.createElement("img");
+      card.querySelector(".leader-art")?.prepend(image);
+    }
+
+    image.src = new URL(source, window.location.href).href;
+    image.alt = normalizedTitle;
+    image.dataset.rasterPortrait = "true";
+  });
+})();
