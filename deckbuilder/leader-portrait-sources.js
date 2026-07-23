11ff4224b3e83f748adcc72cec33eb47b41c37a9
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-07-22 23:40:22 -0400

    Use portable raster sources for later faction portraits

---
+++ deckbuilder/leader-portrait-sources.js
@@ -0,0 +1,27 @@
+(() => {
+  const packages = window.GAUNTLET_V06_SUPPLEMENTALS;
+  if (!packages) return;
+
+  const assetUrl = path => new URL(path, window.location.href).href;
+
+  if (packages.financiers) {
+    packages.financiers.leaderImages = {
+      banker: assetUrl("../images/banker.png"),
+      executive: assetUrl("../images/executive.png")
+    };
+  }
+
+  if (packages.mystics) {
+    packages.mystics.leaderImages = {
+      alchemist: assetUrl("../images/alchemist.png"),
+      "spirit-walker": assetUrl("../images/spirit walker.png")
+    };
+  }
+
+  if (packages.intelligence) {
+    packages.intelligence.leaderImages = {
+      ranger: assetUrl("../images/leader-cards/ranger.jpg"),
+      spymaster: assetUrl("../images/leader-cards/spymaster.jpg")
+    };
+  }
+})();
