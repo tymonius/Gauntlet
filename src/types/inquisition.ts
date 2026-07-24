c23667ca0b95b3d496c235d11518a61b7f165f60
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-07-24 18:04:52 -0400

    Add Inquisition faction state

---
+++ src/types/inquisition.ts
@@ -0,0 +1,6 @@
+export interface InquisitionState {
+  /** Turn on which normal after-battle Conviction was last gained. */
+  convictionBattleGainTurn?: number;
+}
+
+export type PublicInquisitionState = InquisitionState;
