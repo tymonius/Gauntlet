b0a5ff70dae7a716408427ed0cae0bfa237c4409
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-07-26 00:11:06 -0400

    Register Neutral Forced March playability

--- src/cards/playability.ts
+++ src/cards/playability.ts
@@ -39,6 +39,7 @@ export const coreCardPlayRules: Record<CardID, CardPlayRule> = {
   'neutral-contingency-plan': battleAndAction('neutral-contingency-plan', 'asset_bank'),
   'neutral-counterintelligence': battleAndAction('neutral-counterintelligence', 'asset_bank'),
   'neutral-fealty': battleAndAction('neutral-fealty', 'asset_bank'),
+  'neutral-forced-march': battleAndAction('neutral-forced-march', 'discard'),
 
   'card-attrition': battleAndAction('card-attrition', 'asset_bank'),
   'card-conscription': { cardId: 'card-conscription', timings: ['battle_hand_commit', 'battle_draw_play'], allowedOrigins: ['hand', 'battle_draw'], defaultDestinationByOrigin: { hand: 'graveyard', battle_draw: 'discard' } },
