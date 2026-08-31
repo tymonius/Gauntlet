19fc462eb46ca88d6f3d7870f08aceea55429f6a
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-08-31 04:37:30 -0400

    Mark dynamically blank v0.7.0 Territories

--- src/v070/engine.ts
+++ src/v070/engine.ts
@@ -381,6 +381,7 @@ export interface V070BoardTerritory {
   contributedBy: PlayerId;
   controller: PlayerId;
   occupant: PlayerId | null;
+  blank?: boolean;
 }
 
 export interface V070SetupState {
