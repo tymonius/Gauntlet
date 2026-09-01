00cdd822093059301f9b4bfc1656ba94d4c57205
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-09-01 10:23:48 -0400

    Pin apply-reconnaissance.ts to v06 types

--- src/state/apply-reconnaissance.ts
+++ src/state/apply-reconnaissance.ts
@@ -1,4 +1,4 @@
-import type { GameState } from '../types';
+import type { GameState } from '../types/v06';
 import type { AppStateAction } from './actions';
 import { applyGameAction as applyInterceptedOrdersGameAction } from './apply-intercepted-orders';
 import {
