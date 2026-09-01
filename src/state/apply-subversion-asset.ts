40fdbe2da41162eeffb7dcb4af6143fdb9bf7ec7
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-09-01 10:24:13 -0400

    Pin apply-subversion-asset.ts to v06 types

--- src/state/apply-subversion-asset.ts
+++ src/state/apply-subversion-asset.ts
@@ -1,4 +1,4 @@
-import type { GameState } from '../types';
+import type { GameState } from '../types/v06';
 import type { AppStateAction } from './actions';
 import { applyGameAction as applyGraveWardGameAction } from './apply-grave-ward';
 import {
