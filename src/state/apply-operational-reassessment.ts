9a2aeb748d7f43432050365e280223e452a557bf
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-09-01 10:23:33 -0400

    Pin apply-operational-reassessment.ts to v06 types

--- src/state/apply-operational-reassessment.ts
+++ src/state/apply-operational-reassessment.ts
@@ -1,4 +1,4 @@
-import type { GameState } from '../types';
+import type { GameState } from '../types/v06';
 import type { AppStateAction } from './actions';
 import { applyGameAction as applySpiesGameAction } from './apply-spies';
 import {
