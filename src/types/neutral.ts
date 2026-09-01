85f8957e7fd6771005ca26ff0f6218752aedcffd
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-08-31 20:01:38 -0400

    Pin neutral.ts to v06 effects API

--- src/types/neutral.ts
+++ src/types/neutral.ts
@@ -1,4 +1,4 @@
-import type { BattleCardTarget } from '../effects';
+import type { BattleCardTarget } from '../effects/v06';
 import type { CardID, PlayerID, SpaceID } from './ids';
 
 export type DecoysAssetZone = 'asset_bank' | 'hand' | 'discard' | 'graveyard' | 'removed';
