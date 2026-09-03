96804cfa6228d4a1b66aa1db1d570fbd38979691
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-09-03 14:43:50 -0400

    Compose legacy effect handlers statically

--- src/effects/v06.ts
+++ src/effects/v06.ts
@@ -5,20 +5,3 @@ export * from './battle';
 export * from './capital-punishment';
 export * from './disruption';
 export * from './sabotage';
-
-import { baseBattleEffectHandlers } from './battle';
-import { capitalPunishmentCleanupHandler } from './capital-punishment';
-import { disruptionBattleHandler } from './disruption';
-import { sabotageBattleHandler } from './sabotage';
-
-if (!baseBattleEffectHandlers.some((handler) => handler.id === disruptionBattleHandler.id)) {
-  baseBattleEffectHandlers.splice(1, 0, disruptionBattleHandler);
-}
-
-if (!baseBattleEffectHandlers.some((handler) => handler.id === sabotageBattleHandler.id)) {
-  baseBattleEffectHandlers.splice(2, 0, sabotageBattleHandler);
-}
-
-if (!baseBattleEffectHandlers.some((handler) => handler.id === capitalPunishmentCleanupHandler.id)) {
-  baseBattleEffectHandlers.push(capitalPunishmentCleanupHandler);
-}
