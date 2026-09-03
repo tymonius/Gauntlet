dee44b51cccdef1692eb95ca6769631636b34c12
Tymon Scott <54303549+tymonius@users.noreply.github.com>
2026-09-03 01:08:36 -0400

    Declare preparation contract for territory face

--- card-design/face-templates/territory.mjs
+++ card-design/face-templates/territory.mjs
@@ -30,5 +30,5 @@ export function render(spec) {
 
   element.dataset.faceId = spec.id;
   element.dataset.faceTemplate = spec.template;
-  return { element, artworkImage: element.querySelector('.territory-art img') };
+  return { element, artworkImage: element.querySelector('.territory-art img'), preparation: { parchment: 'neutral', fit: 'territory' } };
 }
